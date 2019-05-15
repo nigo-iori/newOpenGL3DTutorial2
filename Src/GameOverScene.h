d469cac24672b52f2503b45b8e6a3a90b4820445
仁娯 庵 <iori_nigo@outlook.jp>
2019-05-15 16:09:01 +0900

    タイトル画面のProcessInput関数、メインゲーム画面のProcessInput関数、ステータス画面のProcessInput関数、ゲームオーバー画面のProcessInput関数を定義

--- Src/GameOverScene.h
+++ Src/GameOverScene.h
@@ -16,9 +16,9 @@ public:
 
 	virtual bool Initialize() override { return true; }
 	virtual void ProcessInput() override;
-	virtual void Update(float) override;
-	virtual void Render() override;
-	virtual void Finalize() override;
+	virtual void Update(float) override {};
+	virtual void Render() override {};
+	virtual void Finalize() override {};
 };
 
 #endif // ! GAMEOVERSCENE_H_INCLUDED
--- Src/TitleScene.h
+++ Src/TitleScene.h
@@ -15,7 +15,7 @@ public:
 	virtual ~TitleScene() = default;
 
 	virtual bool Initialize() override { return true; }
-	virtual void ProcessInput() override {}
+	virtual void ProcessInput() override;
 	virtual void Update(float) override {}
 	virtual void Render() override {}
 	virtual void Finalize() override{}
