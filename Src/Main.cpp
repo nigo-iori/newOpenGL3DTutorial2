6e923a019ab1d12ad4ed5d2d815453c4b7917030
仁娯 庵 <iori_nigo@outlook.jp>
2019-06-08 17:24:16 +0900

    ゲームパッド更新関数を呼び出す ゲームパッド入力でシーンを切り替える

--- Src/Main.cpp
+++ Src/Main.cpp
@@ -3,16 +3,22 @@
 */
 
 #include "TitleScene.h"
+#include "GLFWEW.h"
 
 int main()
 {
+	GLFWEW::Window& window = GLFWEW::Window::Instance();
+	window.Init(1280, 720, u8"アクションゲーム");
+
 	SceneStack& sceneStack = SceneStack::Instance();
 	sceneStack.Push(std::make_shared<TitleScene>());
 
-	for (;;)
-	{
+	while (!window.ShouldClose()) {
+		const float deltaTime = window.DeltaTime();
+		window.UpdateTimer();
 		const float deltaTime = 1.0f / 60.0f;
 		sceneStack.Update(deltaTime);
 		sceneStack.Render();
+		window.SwapBuffers();
 	}
 }
